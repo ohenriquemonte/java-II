19df93649f352008c021cd2e89fd2ab3fd9ed503
Henrique Montenegro <montenegrohenrique@gmail.com>
2019-06-22 21:39:41 -0300

    Aula 02 - Introdução à Orientação a Objetos 2

---
+++ src/CriaConta.java
@@ -0,0 +1,30 @@
+
+public class CriaConta {
+	
+	public static void main(String[] args) {
+		Conta primeiraConta = new Conta();
+		primeiraConta.saldo = 200;
+		primeiraConta.saldo += 100;
+		
+		Conta segundaConta = new Conta();
+		segundaConta.saldo = 50;
+		
+		System.out.println("primeiraConta tem: " + primeiraConta.saldo);
+		System.out.println("segundaConta tem: " + segundaConta.saldo);
+		
+		System.out.println(primeiraConta.agencia);
+		System.out.println(primeiraConta.titular);
+		
+		segundaConta.agencia = 146;
+		System.out.println("Agora a segunda conta esta na agencia " + segundaConta.agencia);
+		
+		if(primeiraConta == segundaConta) {
+			System.out.println("mesma conta");
+		} else {
+			System.out.println("contas diferentes");
+		}
+		
+		System.out.println(primeiraConta);
+		System.out.println(segundaConta);
+	}
+}
